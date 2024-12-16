a6ac944b1d88fcd197418b17f4039e8a5b012bf3
Sameer Shahi Thakuri (Sam) <70689865+Sam231221@users.noreply.github.com>
2024-12-16 11:46:35 +0545

    Modules Upates

--- djangobegineerproject/my_project/my_app1/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Product
-# Register your models here.
-admin.site.register(Product) # This line is added to register the Product model with the admin site.
-
