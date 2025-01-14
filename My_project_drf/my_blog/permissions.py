dcb50f433adaaef4a7e02e05b85e0ba4d3851d78
Sameer Shahi Thakuri (Sam) <70689865+Sam231221@users.noreply.github.com>
2025-01-14 12:59:04 +0545

    more custom permissions defined

--- My_project_drf/my_blog/permissions.py
+++ My_project_drf/my_blog/permissions.py
@@ -1,4 +1,5 @@
 from rest_framework.permissions import BasePermission
+from datetime import datetime
 
 
 class IsAdminOrReadOnly(BasePermission):
@@ -10,3 +11,36 @@ class IsAdminOrReadOnly(BasePermission):
         if request.method in ["GET", "HEAD", "OPTIONS"]:
             return True
         return request.user and request.user.is_staff
+
+
+class IsOwner(BasePermission):
+    """
+    Custom permission to only allow owners of an object to access it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        return obj.owner == request.user
+
+
+class IsInGroup(BasePermission):
+    """
+    Grants access to users in a specific group.
+    """
+
+    group_name = "editors"
+
+    def has_permission(self, request, view):
+        return (
+            request.user.is_authenticated
+            and request.user.groups.filter(name=self.group_name).exists()
+        )
+
+
+class AccessDuringBusinessHours(BasePermission):
+    """
+    Grant access only between 9 AM to 5 PM.
+    """
+
+    def has_permission(self, request, view):
+        current_hour = datetime.now().hour
+        return 9 <= current_hour < 17
